5ce986a6017599f7fb1a81f4b8bd3beddffdb5c5
Ram Alapure <alapureram@gmail.com>
2024-12-30 12:22:16 +0530

    feat: add new grid component
    - Implemented a new grid component for displaying data
    - Added unit tests for the grid component
    - Updated documentation with usage examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import type { ColDef } from 'ag-grid-community';
+import { AllCommunityModule, ModuleRegistry, themeAlpine } from 'ag-grid-community';
+
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+// Row Data Interface
+interface IRow {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [AgGridAngular],
+  template: `
+    <ag-grid-angular 
+    [theme]="theme"
+    style="height: 500px" 
+    [rowData]="rowData" 
+    [columnDefs]="colDefs"
+    [defaultColDef]="defaultColDef" 
+  />
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-grid';
+
+  public theme = themeAlpine;
+
+  // Row Data: The data to be displayed.
+  rowData: IRow[] = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+    { make: "Fiat", model: "500", price: 15774, electric: false },
+    { make: "Nissan", model: "Juke", price: 20675, electric: false },
+  ];
+
+  // Column Definitions: Defines & controls grid columns.
+  colDefs: ColDef<IRow>[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ];
+
+  defaultColDef: ColDef = {
+    flex: 1,
+    resizable: true,
+    sortable: true
+  };
+
 }
